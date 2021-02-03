8a3ef16160c461840efcd215f03ff82078852ec2
Lennin666 <soporte@allnexus.com.co>
2021-02-03 17:25:59 -0500

    implementando useContext para conectar los productos + agregando un contador de productos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Product from './Product';
+import AppContext from '../context/AppContext';
 import '../styles/components/Products.css';
 
-const Products = ({ products }) => (
-  <div className='Products'>
-    <div className='Products-items'>
-      {
-        products.map(product => (
-          <Product key={product.id} product={product} />
-        ))
-      }
+const Products = () => {
+  const { state, addToCart } = useContext(AppContext);
+  const { products } = state;
+
+  // closure: un closure es una función que retorna otra función, la cual recuerda el contexto en el que fue declarada
+  const handleAddToCart = product => () => {
+    addToCart(product)
+  }
+  return (
+    <div className='Products'>
+      <div className='Products-items'>
+        {
+          products.map(product => (
+            <Product key={product.id} product={product} handleAddToCart={handleAddToCart} />
+          ))
+        }
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Products;
